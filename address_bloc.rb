dfe7020edee6d6a75a0c07bd729c12d9b4ad9deb
Jennifer washburn <jjw244@utexas.edu>
2017-04-14 19:02:34 -0500

    Checkpoint 5 - Menu - COMPLETE

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,11 @@
+require_relative 'controllers/menu_controller'
+
+# #4  create new Menu Controller when AddressBloc starts
+menu = MenuController.new
+
+# #5  use system "clear" to clear the ocmmand line
+system "clear"
 puts "Welcome to AddressBloc!"
+
+# #6  call main_menu to display the menu
+menu.main_menu
