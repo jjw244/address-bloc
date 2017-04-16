b8bdca20368ef9b76967a37410363f3b2f04bf0c
Jennifer washburn <jjw244@utexas.edu>
2017-04-16 01:15:44 -0500

    Checkpoint 9 - Menu II - COMPLETE

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -50,43 +50,79 @@ class MenuController
     end
   end
 
-   # #10  stub the rest of the methods called in main_menu
+  # #10  stub the rest of the methods called in main_menu
   def view_all_entries
     # #14  iterate through all entries in AddressBook using each
-     address_book.entries.each do |entry|
-       system "clear"
-       puts entry.to_s
-     # #15  we call entry_submenu to display a submenu for each entry
-       entry_submenu(entry)
-     end
+    address_book.entries.each do |entry|
+      system "clear"
+      puts entry.to_s
+    # #15  we call entry_submenu to display a submenu for each entry
+      entry_submenu(entry)
+    end
 
-     system "clear"
-     puts "End of entries"
+    system "clear"
+    puts "End of entries"
   end
 
   def create_entry
     # #11  clear the screen for before desplaying the create entry prompts
-     system "clear"
-     puts "New AddressBloc Entry"
-     # #12  use print to prompt the user for each entry attribute
-     print "Name: "
-     name = gets.chomp
-     print "Phone number: "
-     phone = gets.chomp
-     print "Email: "
-     email = gets.chomp
+    system "clear"
+    puts "New AddressBloc Entry"
+    # #12  use print to prompt the user for each entry attribute
+    print "Name: "
+    name = gets.chomp
+    print "Phone number: "
+    phone = gets.chomp
+    print "Email: "
+    email = gets.chomp
 
-     # #13  add a new entry to address_book using add_entry to ensure the new entry is added in the proper order
-     address_book.add_entry(name, phone, email)
+    # #13  add a new entry to address_book using add_entry to ensure the new entry is added in the proper order
+    address_book.add_entry(name, phone, email)
 
-     system "clear"
-     puts "New entry created"
+    system "clear"
+    puts "New entry created"
   end
 
   def search_entries
+    # #9  get the name that the user wants to search for and store it in name
+    print "Search by name: "
+    name = gets.chomp
+    # #10  call search on address_book which will either return a match or nil
+    match = address_book.binary_search(name)
+    system "clear"
+    # #  check if search returned a match
+    if match
+      puts match.to_s
+      search_submenu(match)
+    else
+      puts "No match found for #{name}"
+    end
   end
 
   def read_csv
+    # #1  prompt the user to enter a name of a CSV file to import
+    print "Enter CSV file to import: "
+    file_name = gets.chomp
+
+    # #2  check to see if the file name is empty. If it is then we return the user back to the main menu by calling main_menu
+    if file_name.empty?
+      system "clear"
+      puts "No CSV file read"
+      main_menu
+    end
+
+    # #3  import the specified file with import_from_csv on address_book.
+      # We then clear the screen and print the number of entries that were read from the file.
+      # All of these commands are wrapped in a begin/rescue block.
+      # begin will protect the program from crashing if an exception is thrown.
+    begin
+      entry_count = address_book.import_from_csv(file_name).count
+      system "clear"
+      puts "#{entry_count} new entries added from #{file_name}"
+    rescue
+      puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+      read_csv
+    end
   end
 
 
@@ -102,18 +138,75 @@ class MenuController
 
     case selection
     # #18  when the user asks to see the next entry, we can do nothing and control will be returned to view_all_entries
-     when "n"
+    when "n"
     # #19  we'll handle deleting and editing in another checkpoint, for now the user will be shown the next entry
-     when "d"
-     when "e"
+    when "d"
+      # #7  when a user is viewing the submenu and they press d, we call delete_entry
+      delete_entry(entry)
+    when "e"
+      # #8  call edit_entry when a user presses e; then display a sub-menu with  entry_submenu for the entry under edit
+      edit_entry(entry)
+      entry_submenu(entry)
     # #20  we return the user to the main menu
-     when "m"
+    when "m"
+      system "clear"
+      main_menu
+    else
+      system "clear"
+      puts "#{selection} is not a valid input"
+      entry_submenu(entry)
+    end
+  end
+
+  def delete_entry(entry)
+    address_book.entries.delete(entry)
+    puts "#{entry.name} has been deleted"
+  end
+
+  def edit_entry(entry)
+    # #4  perform a series of print statements followed by gets.chomp assignment statements
+    print "Updated name: "
+    name = gets.chomp
+    print "Updated phone number: "
+    phone_number = gets.chomp
+    print "Updated email: "
+    email = gets.chomp
+    # #5  use !attribute.empty? to set attributes on entry only if a valid attribute was read from user input
+    entry.name = name if !name.empty?
+    entry.phone_number = phone_number if !phone_number.empty?
+    entry.email = email if !email.empty?
+    system "clear"
+    # #6  print out entry with the updated attributes
+    puts "Updated entry:"
+    puts entry
+  end
+
+  def search_submenu(entry)
+    # #12  print out the submenu for an entry
+    puts "\nd - delete entry"
+    puts "e - edit this entry"
+    puts "m - return to main menu"
+    # #13  save the user input to selection
+    selection = gets.chomp
+
+    # #14  use a case statement and take a specific action based on user input
+    case selection
+      when "d"
+        system "clear"
+        delete_entry(entry)
+        main_menu
+      when "e"
+        edit_entry(entry)
         system "clear"
         main_menu
-     else
+      when "m"
+        system "clear"
+        main_menu
+      else
         system "clear"
         puts "#{selection} is not a valid input"
-        entry_submenu(entry)
+        puts entry.to_s
+        search_submenu(entry)
     end
   end
 end
